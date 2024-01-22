a797bc62377a0c9c36aa55e9eefc0a9aa1bb7c90
lokesh903 <lavkeshchhatani@gmail.com>
2024-01-23 00:23:31 +0530

    lec 3 layout foundation started

--- 3-layout -foundation/App.js
+++ 3-layout -foundation/App.js
@@ -31,17 +31,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-  const heading1 = React.createElement('h1',{id:'title'},"i am h2")
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [ heading1]
-  );
+  const heading1 = <h1>Namste React</h1>
   
   // create root using createRoot
   const root = ReactDOM.createRoot(document.getElementById("root"));
   // passing react element inside root
-  root.render(container);
+  root.render(heading1);
